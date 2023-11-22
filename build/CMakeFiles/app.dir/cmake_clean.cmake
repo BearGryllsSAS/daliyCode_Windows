0bccc69e517d588090e7abd136bdd69f80d60f20
haibara ai <2296396559@qq.com>
2023-11-22 11:08:51 +0800

    配置cmake环境

---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/code/daliyCode_Windows/daliyCode_Windows/app.exe"
+  "C:/code/daliyCode_Windows/daliyCode_Windows/app.exe.manifest"
+  "C:/code/daliyCode_Windows/daliyCode_Windows/app.pdb"
+  "C:/code/daliyCode_Windows/daliyCode_Windows/libapp.dll.a"
+  "CMakeFiles/app.dir/main.cpp.obj"
+  "CMakeFiles/app.dir/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
