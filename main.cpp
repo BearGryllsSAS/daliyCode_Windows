3f4a28c6197a9bae38ae1171a4a67c8004f35847
haibara ai <2296396559@qq.com>
2023-11-22 11:20:03 +0800

    测试cmake和gdb是否可用

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 0, b = 0;
+
+    for (int i = 0; i < 10; i++) {
+        a++;
+        b++;
+    }
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
